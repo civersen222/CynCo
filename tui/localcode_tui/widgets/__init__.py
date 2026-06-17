8afc718131c433c4283dc6ca5b83ea1673a05f74
civer <civer@localcode.local>
2026-06-16 19:02:37 -0600

    remove: dead TUI widgets file_preview/diff_view/file_tree (no screen mounts them)

--- tui/localcode_tui/widgets/__init__.py
+++ tui/localcode_tui/widgets/__init__.py
@@ -3,10 +3,7 @@ from .chat_panel import ChatPanel
 from .context_bar import ContextBar
 from .approval import ApprovalDialog
 from .tool_activity import ToolActivity
-from .file_preview import FilePreview
 from .context_sidebar import ContextSidebar
-from .diff_view import DiffView
-from .file_tree import FileTree
 from .progress import ProgressList
 from .worker_animation import WorkerAnimation
 from .confidence_bar import ConfidenceBar
